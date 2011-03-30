a61380a1535e10bd5d0b9cd70905a1207b107ac8
Tony Murray <murraytony@gmail.com>
2011-03-30 00:28:42 -0500

    Small cleanups, test CursorLoader.  It is Honeycomb only however...

--- src/org/homelinux/murray/scorekeep/GameHistory.java
+++ src/org/homelinux/murray/scorekeep/GameHistory.java
@@ -13,49 +13,50 @@ import android.database.Cursor;
 import android.widget.Toast;
 
 public class GameHistory extends ListActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.games_list);
-        
-        Cursor cursor = managedQuery(Game.CONTENT_URI, null, null, null, null);
+	private static final String DEBUG_TAG = "ScoreKeep:GameHistory";
 
-        Log.d("ScoreKeep:GameHistory", "Game Uri is: "+Game.CONTENT_URI);
-        if(cursor == null) {
-            Log.d("ScoreKeep:GameHistory", "Cursor is null");
-        } else {
-        	Log.d("ScoreKeep:GameHistory", "cursor.getCount()=" + cursor.getCount());
-            GamesListAdapter gla = new GamesListAdapter(this,cursor);
-            setListAdapter(gla);
-        }
-    }
-    
-    @Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater inflater = getMenuInflater();
-    	inflater.inflate(R.menu.games_list_menu, menu);
-    	return true;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.games_list);
+
+		/* Android 3.0...
+		CursorLoader loader = new CursorLoader(this, Game.CONTENT_URI, null, null, null, null);
+		Cursor cursor = loader.loadInBackground();
+		*/
+		Cursor cursor = managedQuery(Game.CONTENT_URI, null, null, null, null);
+		Log.d(DEBUG_TAG, "Cursor retrieved:"+cursor);
+		
+		GamesListAdapter gla = new GamesListAdapter(this,cursor);
+		setListAdapter(gla);
 	}
-	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.games_list_menu, menu);
+		return true;
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-    	// Handle item selection
-    	switch (item.getItemId()) {
-    	case R.id.new_game:
-    		startActivity(new Intent(this, NewGame.class));
-    	    return true;
-    	case R.id.view_players:
-    		startActivity(new Intent(this, PlayerList.class));
-        	return true;
-    	case R.id.clear_history:
-    		int rows = getContentResolver().delete(Game.CONTENT_URI, null, null);
-    		if(rows>0) {
-    			Toast.makeText(getApplicationContext(), rows+" games deleted!", Toast.LENGTH_SHORT).show();
-    			return true;
-    		}
-    		return false;
-    	default:
-        	return super.onOptionsItemSelected(item);
-    	}
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.new_game:
+			startActivity(new Intent(this, NewGame.class));
+			return true;
+		case R.id.view_players:
+			startActivity(new Intent(this, PlayerList.class));
+			return true;
+		case R.id.clear_history:
+			int rows = getContentResolver().delete(Game.CONTENT_URI, null, null);
+			if(rows>0) {
+				Toast.makeText(getApplicationContext(), rows+" games deleted!", Toast.LENGTH_SHORT).show();
+				return true;
+			}
+			return false;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
--- src/org/homelinux/murray/scorekeep/NewGame.java
+++ src/org/homelinux/murray/scorekeep/NewGame.java
@@ -25,27 +25,27 @@ import android.widget.Toast;
 public class NewGame extends Activity {
 	private static final String DEBUG_TAG = "ScoreKeep:NewGame";
 	private ListView list;
-	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_game);
-        
-        Cursor c = managedQuery(Player.CONTENT_URI, null, null, null, null);
-        list = (ListView) findViewById(R.id.new_game_players_list);
 
-        PlayerAdapter adapter = new PlayerAdapter(this, c);
-        list.setAdapter(adapter);
-        list.setItemsCanFocus(false);
-        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-    }
-    
-    @Override
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_game);
+
+		Cursor c = managedQuery(Player.CONTENT_URI, null, null, null, null);
+		list = (ListView) findViewById(R.id.new_game_players_list);
+
+		PlayerAdapter adapter = new PlayerAdapter(this, c);
+		list.setAdapter(adapter);
+		list.setItemsCanFocus(false);
+		list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-    	MenuInflater inflater = getMenuInflater();
-    	inflater.inflate(R.menu.new_game_menu, menu);
-    	return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.new_game_menu, menu);
+		return true;
 	}
 
 	@Override
@@ -54,43 +54,43 @@ public class NewGame extends Activity {
 		switch (item.getItemId()) {
 		case R.id.start_game:
 			Intent intent = new Intent(this, ScoreCard.class);
-			
+
 			long[] players = list.getCheckedItemIds();
-        	if(players.length < 1) {
-        		Toast.makeText(this,R.string.no_players_selected,Toast.LENGTH_SHORT).show();
-        		return false;
-        	}
-			
+			if(players.length < 1) {
+				Toast.makeText(this,R.string.no_players_selected,Toast.LENGTH_SHORT).show();
+				return false;
+			}
+
 			final TextView gameDesc = (TextView) findViewById(R.id.game_desc);
-        	Uri newGameUri = newGame(gameDesc.getText().toString(), players);
-        	intent.setData(newGameUri);  //set data uri for the new game
+			Uri newGameUri = newGame(gameDesc.getText().toString(), players);
+			intent.setData(newGameUri);  //set data uri for the new game
 			startActivity(intent);
-    	    return true;
-    	case R.id.add_player:
-    		final AlertDialog.Builder alert = new AlertDialog.Builder(this);
-    		alert.setTitle(R.string.new_player);
-    		final EditText input = new EditText(this);
-    		alert.setView(input);
-    		alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-    			public void onClick(DialogInterface dialog, int whichButton) {
-    				String value = input.getText().toString().trim();
-    				addPlayer(value);
-    				
-    			}
-    		});
-    		alert.setNegativeButton(R.string.cancel,
-    				new DialogInterface.OnClickListener() {
-    					public void onClick(DialogInterface dialog, int whichButton) {
-    						dialog.cancel();
-    					}
-    				});
-    		alert.show();
-        	return true;
-    	default:
-        	return super.onOptionsItemSelected(item);
-    	}
+			return true;
+		case R.id.add_player:
+			final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+			alert.setTitle(R.string.new_player);
+			final EditText input = new EditText(this);
+			alert.setView(input);
+			alert.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					String value = input.getText().toString().trim();
+					addPlayer(value);
+
+				}
+			});
+			alert.setNegativeButton(R.string.cancel,
+					new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+					dialog.cancel();
+				}
+			});
+			alert.show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-    
+
 	private Uri addPlayer(String name) {
 		ContentValues content = new ContentValues();
 		content.put(Player.COLUMN_NAME_NAME, name);
@@ -98,7 +98,7 @@ public class NewGame extends Activity {
 		Log.d(DEBUG_TAG, "Add player result Uri: "+result.toString());
 		return result;
 	}
-	
+
 	private Uri newGame(String desc, long[] player_ids) {
 		ContentValues content = new ContentValues();
 		content.put(Game.COLUMN_NAME_DESCRIPTION, desc);
