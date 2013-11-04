08f9541b099c7faca4bb4f748c0cd4ae6188a849
Tony Murray <murraytony@gmail.com>
2013-11-03 21:30:35 -0600

    Initial changes to make use of a drawer layout instead of a dashboard layout.

--- src/com/splashmobileproductions/scorekeep/HomeActivity.java
+++ src/com/splashmobileproductions/scorekeep/HomeActivity.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2011 Tony Murray <murraytony@gmail.com>
+ *  Copyright 2011-2013 Tony Murray <murraytony@gmail.com>
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,10 +18,23 @@ package com.splashmobileproductions.scorekeep;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 public class HomeActivity extends Activity {
@@ -30,17 +43,71 @@ public class HomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_layout);
 
-		FrameLayout fragmentFrame = (FrameLayout) findViewById(R.id.home_fragment_frame);
 
-		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK)
-				== Configuration.SCREENLAYOUT_SIZE_XLARGE) {
-			// on a xlarge screen device ...
-			getLayoutInflater().inflate(R.layout.app_logo, fragmentFrame);
-		} else {
-			// on a non-xlarge screen device (phone, etc)
-			FragmentTransaction ft = getFragmentManager().beginTransaction();
-			ft.add(R.id.home_fragment_frame, new HomeFragment());
-			ft.commit();
-		}
+        final Button newButton = (Button) findViewById(R.id.home_btn_new);
+        newButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, NewGameFragment.class));
+            }
+        });
+        final Button historyButton = (Button) findViewById(R.id.home_btn_history);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, GameHistoryActivity.class));
+            }
+        });
 	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.home_menu_new:
+                startActivity(new Intent(this, NewGameFragment.class));
+                return true;
+            case R.id.home_menu_about:
+                //show about dialog
+                try {
+                    AboutDialogBuilder.create(this).show();
+                } catch (PackageManager.NameNotFoundException nnfe) {}
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    public static class AboutDialogBuilder {
+        public static AlertDialog create( Context context ) throws PackageManager.NameNotFoundException {
+            // Try to load the a package matching the name of our own package
+            PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            String versionInfo = pInfo.versionName;
+
+            String aboutTitle = String.format("About %s", context.getString(R.string.app_name));
+            String versionString = String.format("Version: %s", versionInfo);
+            String aboutText = context.getString(R.string.about_dialog_text);
+
+            // Set up the TextView
+            final TextView message = new TextView(context);
+            // We'll use a spannable string to be able to make links clickable
+            final SpannableString s = new SpannableString(aboutText);
+
+            // Set some padding
+            message.setPadding(5, 5, 5, 5);
+            // Set up the final string
+            message.setText(versionString + "\n\n" + s);
+            // Now linkify the text
+            Linkify.addLinks(message, Linkify.ALL);
+
+            return new AlertDialog.Builder(context).setTitle(aboutTitle).setCancelable(true).setIcon(R.drawable.icon).setPositiveButton(
+                    context.getString(android.R.string.ok), null).setView(message).create();
+        }
+    }
+
 }
